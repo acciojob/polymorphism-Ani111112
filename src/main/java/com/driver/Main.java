6910718cdb60f17cb7fef6fc49554840bc2c0d25
Aniruddha <aniruddhamukherjee232@gmail.com>
2023-05-17 11:27:01 +0530

    final changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product(5 , 10);
-        p = new Product(5, 5, 5);
-        p = new Product(5.5, 5.6);
+        p.getInt();
+        p = new Product(10.20, 20.00);
+        p.getDouble();
     }
 }
--- src/main/java/com/driver/Product.java
+++ src/main/java/com/driver/Product.java
@@ -2,21 +2,23 @@ package com.driver;
 
 public class Product {
     int x, y, z;
-    double a, b;
+    double a, b, c;
 
     public Product(int x, int y) {
         this.x = x;
         this.y = y;
+        this.z = x + y;
     }
-
-    public Product(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    void getInt(){
+        System.out.println(this.z);
     }
 
     public Product(double a, double b) {
         this.a = a;
         this.b = b;
+        this.c = a + b;
+    }
+    void getDouble(){
+        System.out.println(this.c);
     }
 }
