53f3548795db46a614f32aa0bae3aa5e83705553
Aniruddha <aniruddhamukherjee232@gmail.com>
2023-05-17 11:31:40 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product(5 , 10);
         p.getInt();
-        p = new Product(10.20, 20.00);
         p.getDouble();
     }
 }
